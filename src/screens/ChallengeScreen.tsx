db9cb26c76af9759e9bbc6eac9b468e70bfb37c4
Liam Franke <liam.franke@yahoo.ca>
2021-01-17 00:13:11 -0800

    Leaderboard Modal Set

--- src/screens/ChallengeScreen.tsx
+++ src/screens/ChallengeScreen.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
-import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+    Modal,
+    Button,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    View,
+    LogBox,
+    ImageBackground,
+} from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import ViewPager from '@react-native-community/viewpager';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {
+    ScrollView,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+} from 'react-native-gesture-handler';
 import {
     Body,
-    Button,
     Card,
     CardItem,
     Container,
@@ -18,6 +30,7 @@ import {
     Thumbnail,
 } from 'native-base';
 import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
+import { Table, Row, Rows } from 'react-native-table-component';
 interface Challenge {
     posterUri: string;
     type: string;
@@ -59,6 +72,42 @@ const savedChallenges: Challenge[] = [
     },
 ];
 
+const LeaderBoard: React.FC<Props> = ({ challenge }: Props) => {
+    const tableHead = ['#', 'User', 'Views', 'Likes'];
+    const tableData = [
+        ['1', 'Logan', '2000', '123'],
+        ['2', 'Dana', '1500', '12'],
+        ['3', 'Liam', '1000', '9'],
+        ['4', 'Simon', '100', '6'],
+    ];
+    return (
+        <View style={styles.modalContainer}>
+            <View
+                style={{
+                    position: 'absolute',
+                    left: 5,
+                    top: 20,
+                }}
+            ></View>
+            <Text
+                style={{
+                    textAlign: 'center',
+                    fontSize: 40,
+                    textDecorationLine: 'underline',
+                }}
+            >
+                {challenge.title}
+            </Text>
+            <View style={styles.container}>
+                <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                    <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+                    <Rows data={tableData} textStyle={styles.text} />
+                </Table>
+            </View>
+        </View>
+    );
+};
+
 const SaveChallengeButton: React.FC<Props> = ({ challenge }: Props) => {
     const [saved, setSaved] = useState(false);
     function saveChallenge() {
@@ -80,47 +129,59 @@ const SaveChallengeButton: React.FC<Props> = ({ challenge }: Props) => {
         </TouchableOpacity>
     );
 };
+
 const FeedChallenge: React.FC<Props> = ({ challenge }: Props) => {
+    const [ModalOpen, setModalOpen] = useState(false);
     return (
-        <Card style={{ flex: 1 }}>
-            <CardItem cardBody style={{ flex: 1 }}>
-                <Image
-                    resizeMode="cover"
-                    style={{ height: '100%', flex: 1 }}
-                    source={{ uri: challenge.posterUri }}
-                />
-            </CardItem>
-            <CardItem bordered>
-                <Left>
-                    <Text>{challenge.title}</Text>
-                    <Text note>{challenge.type}</Text>
-                </Left>
-                <Right>
-                    <FontAwesome5
-                        style={styles.sendButton}
-                        name="share"
-                        light
-                        color="green"
-                        size={18}
+        <>
+            <Modal animationType="slide" visible={ModalOpen}>
+                <ScrollView>
+                    <View style={{ position: 'absolute', top: 20, left: 20 }}>
+                        <Button title="X" onPress={() => setModalOpen(false)} />
+                    </View>
+                    <LeaderBoard challenge={challenge} />
+                </ScrollView>
+            </Modal>
+            <Card style={{ flex: 1 }}>
+                <CardItem cardBody style={{ flex: 1 }}>
+                    <ImageBackground
+                        resizeMode="cover"
+                        style={{ height: '100%', flex: 1, width: '100%' }}
+                        source={{ uri: challenge.posterUri }}
                     />
-                </Right>
-            </CardItem>
-            <CardItem footer>
-                <Left>
-                    <FontAwesome5
-                        style={styles.userCount}
-                        name="users"
-                        light
-                        color="black"
-                        size={15}
-                    />
-                    <Text>{challenge.userCount}</Text>
-                </Left>
-                <Right>
-                    <SaveChallengeButton challenge={challenge} />
-                </Right>
-            </CardItem>
-        </Card>
+                </CardItem>
+                <CardItem bordered>
+                    <Left>
+                        <Text onPress={() => setModalOpen(true)}>{challenge.title}</Text>
+                        <Text note>{challenge.type}</Text>
+                    </Left>
+                    <Right>
+                        <FontAwesome5
+                            style={styles.sendButton}
+                            name="share"
+                            light
+                            color="green"
+                            size={18}
+                        />
+                    </Right>
+                </CardItem>
+                <CardItem footer>
+                    <Left>
+                        <FontAwesome5
+                            style={styles.userCount}
+                            name="users"
+                            light
+                            color="black"
+                            size={15}
+                        />
+                        <Text>{challenge.userCount}</Text>
+                    </Left>
+                    <Right>
+                        <SaveChallengeButton challenge={challenge} />
+                    </Right>
+                </CardItem>
+            </Card>
+        </>
     );
 };
 
@@ -154,6 +215,7 @@ const BrowseChallengeScreen: React.FC = () => {
             userCount: 200,
         },
     ];
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <ViewPager style={{ flex: 1 }} initialPage={0}>
@@ -241,6 +303,24 @@ const styles = StyleSheet.create({
     userCount: {
         // padding: 5,
         fontWeight: 'bold',
+    },
+    modalContainer: {
+        backgroundColor: '#fff',
+        paddingHorizontal: 30,
+        marginTop: 60,
+    },
+    container: {
+        flex: 1,
+        padding: 16,
+        paddingTop: 30,
+        backgroundColor: '#fff',
+    },
+    head: {
+        height: 40,
+        backgroundColor: '#f1f8ff',
+    },
+    text: {
+        margin: 6,
     },
 });
 export default ChallengeNavigator;
